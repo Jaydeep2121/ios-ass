4304ffe08f4ae70e544aaf648f936b9cd57281d8
Jaydeep Chauhan <66904825+Jaydeep2121@users.noreply.github.com>
2021-12-11 09:57:11 +0530

    11/12/2021
    ass-9 complete

--- Ass9/Ass9/ViewController.swift
+++ Ass9/Ass9/ViewController.swift
@@ -15,36 +15,63 @@ class ViewController: UIViewController {
         let imgview = UIImageView()
         imgview.contentMode = .scaleAspectFill
         imgview.backgroundColor = .gray
-        imgview.image = UIImage(named: "bg_UI")
         imgview.clipsToBounds = true
         return imgview
     }()
-    private let Upbtn : UIButton = {
-        let btn = UIButton()
-        btn.setTitle("Select Image", for: .normal)
-        btn.backgroundColor = .lightGray
-        btn.addTarget(self, action: #selector(Upload), for: .touchUpInside)
-        return btn
+    private let titlelabel:UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.text = "Tap Below To select Image"
+        label.textColor = .green
+        label.font = UIFont.italicSystemFont(ofSize: 30)
+        label.font = UIFont(name: "HelveticaNeue-UltraLight", size: 30)
+        return label
     }()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addSubview(titlelabel)
         view.addSubview(imgview)
-        view.addSubview(Upbtn)
         imgpicker.delegate = self
-    }
-    
-    @objc func Upload(){
-        imgpicker.sourceType = .photoLibrary
-        DispatchQueue.main.async {
-            self.present(self.imgpicker, animated: true)
-        }
+        
+        /*Tap Gestures*/
+        let tapgesture = UITapGestureRecognizer(target: self, action: #selector(tapg))
+        tapgesture.numberOfTapsRequired = 1
+        tapgesture.numberOfTouchesRequired = 1
+        view.addGestureRecognizer(tapgesture)
+        
+        /*Pinch Gestures*/
+        let pinchgesture = UIPinchGestureRecognizer(target: self, action: #selector(didpinch))
+        view.addGestureRecognizer(pinchgesture)
+        
+        /*Rotate Gesture*/
+         let rotationGesture = UIRotationGestureRecognizer(target: self, action: #selector(didrotate))
+         view.addGestureRecognizer(rotationGesture)
+ 
+        /*Swipe Gesture*/
+        let leftswipe = UISwipeGestureRecognizer(target: self, action: #selector(didSwipe))
+        leftswipe.direction = .left
+        view.addGestureRecognizer(leftswipe)
+
+        let rightswipe = UISwipeGestureRecognizer(target: self, action: #selector(didSwipe))
+        rightswipe.direction = .right
+        view.addGestureRecognizer(rightswipe)
+
+        let upswipe = UISwipeGestureRecognizer(target: self, action: #selector(didSwipe))
+        upswipe.direction = .up
+        view.addGestureRecognizer(upswipe)
+
+        let downswipe = UISwipeGestureRecognizer(target: self, action: #selector(didSwipe))
+        downswipe.direction = .down
+        view.addGestureRecognizer(downswipe)
+        
+        let panGestures = UIPanGestureRecognizer(target: self, action: #selector(didpanview))
+        view.addGestureRecognizer(panGestures)
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        imgview.frame = CGRect(x: 20, y:100, width: 80, height: 100)
-        Upbtn.frame = CGRect(x: 100, y: view.bottom-50, width: view.width-200, height: 40)
+        titlelabel.frame = CGRect(x: 30, y: 50, width: view.width-30, height: 50)
+        imgview.frame = CGRect(x: 70, y:170, width: 200, height: 200)
     }
 }
 extension ViewController : UIImagePickerControllerDelegate,UINavigationControllerDelegate{
@@ -53,5 +80,43 @@ extension ViewController : UIImagePickerControllerDelegate,UINavigationControlle
             imgview.image = getimage
         }
         imgpicker.dismiss(animated: true)
+    }
+    @objc private func tapg(gesture:UITapGestureRecognizer){
+        imgpicker.sourceType = .photoLibrary
+        DispatchQueue.main.async {
+            self.present(self.imgpicker, animated: true)
+        }
+    }
+    @objc private func didSwipe(gesture: UISwipeGestureRecognizer){
+        if gesture.direction == .left{
+            UIView.animate(withDuration: 0.5){
+                self.imgview.frame = CGRect(x: self.imgview.frame.origin.x-40, y: self.imgview.frame.origin.y, width: 200, height: 200)
+            }
+        }else if gesture.direction == .right{
+            UIView.animate(withDuration: 0.5){
+                self.imgview.frame = CGRect(x: self.imgview.frame.origin.x+40, y: self.imgview.frame.origin.y, width: 200, height: 200)
+            }
+        }else if gesture.direction == .up{
+            UIView.animate(withDuration: 0.5){
+                self.imgview.frame = CGRect(x: self.imgview.frame.origin.x, y: self.imgview.frame.origin.y-40, width: 200, height: 200)
+            }
+        }else if gesture.direction == .down{
+            UIView.animate(withDuration: 0.5){
+                self.imgview.frame = CGRect(x: self.imgview.frame.origin.x, y: self.imgview.frame.origin.y+40, width: 200, height: 200)
+            }
+        }
+    }
+    @objc private func didpinch(gesture:UIPinchGestureRecognizer){
+        imgview.transform = CGAffineTransform(scaleX: gesture.scale, y: gesture.scale)
+    }
+    @objc private func didrotate(gesture:UIRotationGestureRecognizer){
+        imgview.transform = CGAffineTransform(rotationAngle: gesture.rotation)
+    }
+    
+    @objc private func didpanview(gesture:UIPanGestureRecognizer){
+        let x = gesture.location(in: view).x
+        let y = gesture.location(in: view).y
+        
+        imgview.center = CGPoint(x: x, y: y)
     }
 }
