67af81636e79ced9a8f1de87ddf263e135e0da26
Jaydeep Chauhan <66904825+Jaydeep2121@users.noreply.github.com>
2021-12-14 17:47:53 +0530

    14/12/2021

---
+++ Ass11/Ass11/LoginVC.swift
@@ -0,0 +1,97 @@
+//
+//  ViewController.swift
+//  Ass11
+//
+//  Created by DCS on 14/12/21.
+//  Copyright © 2021 DCS. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+    private let titlelabel:UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.text = "Proceed with your"
+        label.font = UIFont.italicSystemFont(ofSize: 20)
+        label.font = UIFont(name: "HelveticaNeue", size: 20)
+        return label
+    }()
+    private let loginlabel:UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.text = "Login"
+        label.font = UIFont.italicSystemFont(ofSize: 25)
+        label.font = UIFont(name: "HelveticaNeue-Bold", size: 25)
+        return label
+    }()
+    private let forgetlabel:UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.text = "Forgot Password?"
+        label.font = UIFont.italicSystemFont(ofSize: 10)
+        label.font = UIFont(name: "HelveticaNeue", size: 10)
+        return label
+    }()
+    private let usertext:UITextField = {
+        let textfield = UITextField()
+        textfield.textColor = .black
+        textfield.placeholder = "Username"
+        textfield.textAlignment = .center
+        textfield.layer.cornerRadius = 15.0
+        textfield.backgroundColor = .white
+        textfield.layer.borderWidth = 0
+        textfield.layer.shadowColor = UIColor.black.cgColor
+        textfield.layer.shadowOffset = CGSize(width: 3, height: 3)
+        textfield.layer.shadowOpacity = 0.7
+        textfield.layer.shadowRadius = 4.0
+        textfield.font = UIFont.init(name: "Montserrt.bold.rawValue", size: 18.0)
+        return textfield
+    }()
+    private let passtext:UITextField = {
+        let textfield = UITextField()
+        textfield.textColor = .black
+        textfield.placeholder = "Password"
+        textfield.textAlignment = .center
+        textfield.layer.cornerRadius = 15.0
+        textfield.backgroundColor = .white
+        textfield.layer.borderWidth = 0
+        textfield.layer.shadowColor = UIColor.black.cgColor
+        textfield.layer.shadowOffset = CGSize(width: 3, height: 3)
+        textfield.layer.shadowOpacity = 0.7
+        textfield.layer.shadowRadius = 4.0
+        textfield.font = UIFont.init(name: "Montserrt.bold.rawValue", size: 18.0)
+        return textfield
+    }()
+    private let loginbutton:UIButton = {
+        let button = UIButton()
+        button.setTitle("Login", for: .normal)
+        button.addTarget(self, action: #selector(loginClickbtn), for: .touchUpInside)
+        button.tintColor = .white
+        button.backgroundColor = .red
+        return button
+    }()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //self.view.backgroundColor = UIColor(patternImage: UIImage(named: "image1.jpg")!)
+        view.addSubview(titlelabel)
+        view.addSubview(loginlabel)
+        view.addSubview(forgetlabel)
+        view.addSubview(usertext)
+        view.addSubview(passtext)
+        view.addSubview(loginbutton)
+    }
+    @objc func loginClickbtn(){
+        print("clicked...")
+    }
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        titlelabel.frame = CGRect(x: 5, y: 200, width: view.width-150, height: 50)
+        loginlabel.frame = CGRect(x: 5, y: 230, width: view.width-245, height: 50)
+        usertext.frame = CGRect(x: 37, y: loginlabel.frame.origin.y+90, width: view.width-100, height: 50)
+        passtext.frame = CGRect(x: 37, y: usertext.frame.origin.y+70, width: view.width-100, height: 50)
+        loginbutton.frame = CGRect(x: 37, y: passtext.frame.origin.y+70, width: view.width-100, height: 50)
+        forgetlabel.frame = CGRect(x: 40, y: loginbutton.frame.origin.y+50, width: view.width-100, height: 30)
+    }
+}
+
---
+++ Ass11/Ass11/Regpage.swift
@@ -0,0 +1,125 @@
+//
+//  Regpage.swift
+//  Ass11
+//
+//  Created by DCS on 14/12/21.
+//  Copyright © 2021 DCS. All rights reserved.
+//
+
+import UIKit
+
+class Regpage: UIViewController {
+    private let titlelabel:UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.text = "Registration"
+        label.font = UIFont.italicSystemFont(ofSize: 50)
+        label.font = UIFont(name: "HelveticaNeue-UltraLight", size: 50)
+        return label
+    }()
+    private let spidtextfield:UITextField = {
+        let textfield = UITextField()
+        textfield.placeholder = "Enter SPID"
+        textfield.layer.borderWidth = 1
+        let img = UIImageView(frame: CGRect(x: 20, y: 50, width: 40, height: 20))
+        img.contentMode = .scaleAspectFit
+        img.image = #imageLiteral(resourceName: "id")
+        textfield.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 15, height: textfield.frame.height))
+        textfield.leftViewMode = .always
+        textfield.rightView = img
+        textfield.rightViewMode = .always
+        return textfield
+    }()
+    private let nametextfield:UITextField = {
+        let textfield = UITextField()
+        textfield.placeholder = "Enter Name"
+        let img = UIImageView(frame: CGRect(x: 20, y: 50, width: 40, height: 20))
+        img.contentMode = .scaleAspectFit
+        img.image = #imageLiteral(resourceName: "contact")
+        textfield.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 15, height: textfield.frame.height))
+        textfield.leftViewMode = .always
+        textfield.rightView = img
+        textfield.rightViewMode = .always
+        textfield.layer.borderWidth = 1
+        return textfield
+    }()
+    private let emailtextfield:UITextField = {
+        let textfield = UITextField()
+        textfield.placeholder = "Enter Email"
+        textfield.layer.borderWidth = 1
+        let img = UIImageView(frame: CGRect(x: 20, y: 50, width: 40, height: 20))
+        img.contentMode = .scaleAspectFit
+        img.image = #imageLiteral(resourceName: "email")
+        textfield.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 15, height: textfield.frame.height))
+        textfield.leftViewMode = .always
+        textfield.rightView = img
+        textfield.rightViewMode = .always
+        return textfield
+    }()
+    private let passtextfield:UITextField = {
+        let textfield = UITextField()
+        textfield.placeholder = "Enter Password"
+        let img = UIImageView(frame: CGRect(x: 20, y: 50, width: 40, height: 20))
+        img.contentMode = .scaleAspectFit
+        img.image = #imageLiteral(resourceName: "key")
+        textfield.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 15, height: textfield.frame.height))
+        textfield.leftViewMode = .always
+        textfield.rightView = img
+        textfield.rightViewMode = .always
+        textfield.layer.borderWidth = 1
+        return textfield
+    }()
+    private let mysegment : UISegmentedControl = {
+       let sg = UISegmentedControl()
+        sg.insertSegment(withTitle: "Male", at: 0, animated: true)
+        sg.insertSegment(withTitle: "Female", at: 1, animated: true)
+        sg.insertSegment(withTitle: "Other", at: 2, animated: true)
+        return sg
+    }()
+    private let mysegment1 : UISegmentedControl = {
+        let sg = UISegmentedControl()
+        sg.insertSegment(withTitle: "Bcom", at: 0, animated: true)
+        sg.insertSegment(withTitle: "BCA", at: 1, animated: true)
+        sg.insertSegment(withTitle: "BBA", at: 2, animated: true)
+        sg.insertSegment(withTitle: "BSc", at: 3, animated: true)
+        sg.insertSegment(withTitle: "CA", at: 4, animated: true)
+        return sg
+    }()
+    private let savebutton:UIButton = {
+        let button = UIButton()
+        button.setTitle("SAVE", for: .normal)
+        button.addTarget(self, action: #selector(savenote), for: .touchUpInside)
+        button.tintColor = .white
+        button.backgroundColor = .green
+        button.layer.cornerRadius = 6
+        return button
+    }()
+    override func viewDidLoad() {
+        view.addSubview(titlelabel)
+        view.addSubview(spidtextfield)
+        view.addSubview(nametextfield)
+        view.addSubview(emailtextfield)
+        view.addSubview(passtextfield)
+        view.addSubview(mysegment)
+        view.addSubview(mysegment1)
+        view.addSubview(savebutton)
+        super.viewDidLoad()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        titlelabel.frame = CGRect(x: 10, y: 20, width: view.width, height: 100)
+        spidtextfield.frame = CGRect(x: 30, y: titlelabel.bottom+20, width: view.width-60, height: 40)
+        nametextfield.frame = CGRect(x: 30, y: spidtextfield.bottom + 20, width: view.width-60, height: 40)
+        emailtextfield.frame = CGRect(x: 30, y: nametextfield.bottom + 20, width: view.width-60, height: 40)
+        mysegment.frame = CGRect(x: 30, y: emailtextfield.bottom+10, width: view.width-60, height: 30)
+        passtextfield.frame = CGRect(x: 30, y: mysegment.bottom+10, width: view.width-60, height: 40)
+        mysegment1.frame = CGRect(x: 30, y: passtextfield.bottom+10, width: view.width-60, height: 30)
+        savebutton.frame = CGRect(x: 30, y: mysegment1.bottom+30, width: view.width-60, height: 40)
+    }
+}
+extension Regpage{
+    @objc func savenote(){
+        print("click")
+    }
+}
