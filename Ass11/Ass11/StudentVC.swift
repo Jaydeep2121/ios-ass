51b696bc33873da416499e575cb01e42ff38ec46
Jaydeep Chauhan <66904825+Jaydeep2121@users.noreply.github.com>
2021-12-20 10:29:53 +0530

    monday

---
+++ Ass11/Ass11/StudentVC.swift
@@ -0,0 +1,48 @@
+//
+//  StudentVC.swift
+//  Ass11
+//
+//  Created by DCS on 20/12/21.
+//  Copyright © 2021 DCS. All rights reserved.
+//
+import UIKit
+import Foundation
+class StudentVC: UIViewController {
+    private let studviewbtn:UIButton = {
+        let button = UIButton()
+        button.setTitle("View Profile", for: .normal)
+        button.addTarget(self, action: #selector(viewprofile), for: .touchUpInside)
+        button.tintColor = .white
+        button.backgroundColor = .green
+        button.layer.cornerRadius = 6
+        return button
+    }()
+    private let noticeviewbtn:UIButton = {
+        let button = UIButton()
+        button.setTitle("View NoticeBoard", for: .normal)
+        button.addTarget(self, action: #selector(viewnotice), for: .touchUpInside)
+        button.tintColor = .white
+        button.backgroundColor = .green
+        button.layer.cornerRadius = 6
+        return button
+    }()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(studviewbtn)
+        view.addSubview(noticeviewbtn)
+    }
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        studviewbtn.frame = CGRect(x: 90, y: 200, width: 200, height: 70)
+        noticeviewbtn.frame = CGRect(x: 90, y: studviewbtn.bottom+20, width: 200, height: 70)
+    }
+}
+extension StudentVC{
+    @objc func viewprofile(){
+        let vc = ViewStudentData()
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    @objc func viewnotice(){
+        print("hello view notice")
+    }
+}
--- Ass11/Ass11/changepwd.swift
+++ Ass11/Ass11/changepwd.swift
@@ -8,10 +8,7 @@
 
 import UIKit
 
-class LoginVC: UIViewController {
-    var Student:stud?
-    let uname = "Admin"
-    let upass = "admin123"
+class changepwd: UIViewController {
     private let titlelabel:UILabel = {
         let label = UILabel()
         label.textAlignment = .center
@@ -23,35 +20,12 @@ class LoginVC: UIViewController {
     private let loginlabel:UILabel = {
         let label = UILabel()
         label.textAlignment = .center
-        label.text = "Login"
+        label.text = "Change Password"
         label.font = UIFont.italicSystemFont(ofSize: 25)
         label.font = UIFont(name: "HelveticaNeue-Bold", size: 25)
         return label
     }()
-    private let forgetlabel:UILabel = {
-        let label = UILabel()
-        label.textAlignment = .center
-        label.text = "Forgot Password?"
-        label.font = UIFont.italicSystemFont(ofSize: 10)
-        label.font = UIFont(name: "HelveticaNeue", size: 10)
-        return label
-    }()
     private let usertext:UITextField = {
-        let textfield = UITextField()
-        textfield.textColor = .black
-        textfield.placeholder = "Username"
-        textfield.textAlignment = .center
-        textfield.layer.cornerRadius = 15.0
-        textfield.backgroundColor = .white
-        textfield.layer.borderWidth = 0
-        textfield.layer.shadowColor = UIColor.black.cgColor
-        textfield.layer.shadowOffset = CGSize(width: 3, height: 3)
-        textfield.layer.shadowOpacity = 0.7
-        textfield.layer.shadowRadius = 4.0
-        textfield.font = UIFont.init(name: "Montserrt.bold.rawValue", size: 18.0)
-        return textfield
-    }()
-    private let passtext:UITextField = {
         let textfield = UITextField()
         textfield.textColor = .black
         textfield.placeholder = "Password"
@@ -66,10 +40,25 @@ class LoginVC: UIViewController {
         textfield.font = UIFont.init(name: "Montserrt.bold.rawValue", size: 18.0)
         return textfield
     }()
+    private let passtext:UITextField = {
+        let textfield = UITextField()
+        textfield.textColor = .black
+        textfield.placeholder = "Confirm Password"
+        textfield.textAlignment = .center
+        textfield.layer.cornerRadius = 15.0
+        textfield.backgroundColor = .white
+        textfield.layer.borderWidth = 0
+        textfield.layer.shadowColor = UIColor.black.cgColor
+        textfield.layer.shadowOffset = CGSize(width: 3, height: 3)
+        textfield.layer.shadowOpacity = 0.7
+        textfield.layer.shadowRadius = 4.0
+        textfield.font = UIFont.init(name: "Montserrt.bold.rawValue", size: 18.0)
+        return textfield
+    }()
     private let loginbutton:UIButton = {
         let button = UIButton()
-        button.setTitle("Login", for: .normal)
-        button.addTarget(self, action: #selector(loginClickbtn), for: .touchUpInside)
+        button.setTitle("Change", for: .normal)
+        button.addTarget(self, action: #selector(ChangeClickbtn), for: .touchUpInside)
         button.tintColor = .white
         button.backgroundColor = .red
         return button
@@ -79,49 +68,48 @@ class LoginVC: UIViewController {
         //self.view.backgroundColor = UIColor(patternImage: UIImage(named: "image1.jpg")!)
         view.addSubview(titlelabel)
         view.addSubview(loginlabel)
-        view.addSubview(forgetlabel)
         view.addSubview(usertext)
         view.addSubview(passtext)
         view.addSubview(loginbutton)
     }
-    @objc func loginClickbtn(){
+    @objc func ChangeClickbtn(){
+        print("hi change passwd")
         //let sc = liststud()
         //navigationController?.pushViewController(sc, animated: true)
-//        if usertext.text == uname && passtext.text == upass {
-//            let sc = liststud()
-//            navigationController?.pushViewController(sc, animated: true)
-//        }else{
-//            alert = UIAlertController(title: "Failed to LoggedIn", message: "Incorrect Username OR Password", preferredStyle: UIAlertController.Style.alert)
-//            alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
-//            self.present(alert, animated: true, completion: nil)
-//        }
+        //        if usertext.text == uname && passtext.text == upass {
+        //            let sc = liststud()
+        //            navigationController?.pushViewController(sc, animated: true)
+        //        }else{
+        //            alert = UIAlertController(title: "Failed to LoggedIn", message: "Incorrect Username OR Password", preferredStyle: UIAlertController.Style.alert)
+        //            alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        //            self.present(alert, animated: true, completion: nil)
+        //        }
         
-          
-//        var alert = UIAlertController()
-//        let sid = Int(usertext.text!) ?? 0
-//        let id = SqliteHandler.shared.fetchid(id: sid) {_ in}
-//        if Int(usertext.text!) == sid &&  Int(passtext.text!) == sid{
-//            print("log in")
-//            print(id)
-//        }else if(usertext.text == uname && passtext.text == upass){
-//            let sc = liststud()
-//            navigationController?.pushViewController(sc, animated: true)
-//        }else{
-//            alert = UIAlertController(title: "Failed to LoggedIn", message: "Incorrect Username OR Password", preferredStyle: UIAlertController.Style.alert)
-//            alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
-//            self.present(alert, animated: true, completion: nil)
-//            usertext.text=""
-//            passtext.text=""
-//        }
+        
+        //        var alert = UIAlertController()
+        //        let sid = Int(usertext.text!) ?? 0
+        //        let id = SqliteHandler.shared.fetchid(id: sid) {_ in}
+        //        if Int(usertext.text!) == sid &&  Int(passtext.text!) == sid{
+        //            print("log in")
+        //            print(id)
+        //        }else if(usertext.text == uname && passtext.text == upass){
+        //            let sc = liststud()
+        //            navigationController?.pushViewController(sc, animated: true)
+        //        }else{
+        //            alert = UIAlertController(title: "Failed to LoggedIn", message: "Incorrect Username OR Password", preferredStyle: UIAlertController.Style.alert)
+        //            alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        //            self.present(alert, animated: true, completion: nil)
+        //            usertext.text=""
+        //            passtext.text=""
+        //        }
     }
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         titlelabel.frame = CGRect(x: 5, y: 200, width: view.width-150, height: 50)
-        loginlabel.frame = CGRect(x: 5, y: 230, width: view.width-245, height: 50)
+        loginlabel.frame = CGRect(x: 5, y: 230, width: view.width-100, height: 50)
         usertext.frame = CGRect(x: 37, y: loginlabel.frame.origin.y+90, width: view.width-100, height: 50)
         passtext.frame = CGRect(x: 37, y: usertext.frame.origin.y+70, width: view.width-100, height: 50)
         loginbutton.frame = CGRect(x: 37, y: passtext.frame.origin.y+70, width: view.width-100, height: 50)
-        forgetlabel.frame = CGRect(x: 40, y: loginbutton.frame.origin.y+50, width: view.width-100, height: 30)
     }
 }
 
